c4f1bf8f308bf5080b6abe5c5665be2965cc0efb
Z3RØ_AN7RÄX <70908677+js13-Hacker@users.noreply.github.com>
2020-11-24 22:01:20 -0300

    Add files via upload

---
+++ ToolInstaller.sh
@@ -0,0 +1,149 @@
+#!/bin/bash
+COR_CINZA="\033[1;30m"
+COR_VERMELHO="\033[0;31m"
+COR_VERMELHO_CLARO="\033[1;31m"
+COR_AZUL_ESCURO="\033[0;34m"
+COR_AZUL_MARINHO="\033[1;34m"
+COR_AMARELO="\033[1;33m"
+COR_VERDE='\033[1;32m'
+
+clear
+printf $COR_VERDE
+echo "===================================="
+echo  "[•]Created by:Z3RØ_AN7RÄX"
+echo "===================================="
+printf $COR_AMARELO
+echo "           ┌∩┐(◣_◢)┌∩┐"
+printf $COR_VERDE
+echo "===================================="
+echo "[•]Isso pode demorar alguns minutos" 
+echo "===================================="
+pkg install util-linux -y
+setterm -foreground green -store
+
+pkg upgrade && pkg update -y
+
+pkg install curl -y
+
+pkg install wget -y
+
+pkg install lolcat -y
+
+pkg install python -y
+
+pkg install python2 -y
+
+pkg install python3 -y
+
+pkg install pip -y
+
+pkg install pip2 -y 
+
+pkg install irssi -y
+
+pkg install php -y
+
+pkg install git -y
+
+pkg install openssh -y
+
+git clone https://github.com/cyweb/hammer
+mv hammer /data/data/com.termux/files/home
+git clone https://github.com/Gameye98/Lazymux
+mv Lazymux /data/data/com.termux/files/home
+git clone https://github.com/LimerBoy/Impulse
+mv Impulse /data/data/com.termux/files/home
+git clone https://github.com/AngelSecurityTeam/Cam-Hackers
+mv Cam-Hackers /data/data/com.termux/files/home
+git clone https://github.com/TheReaper167/Malicious
+mv Malicious /data/data/com.termux/files/home
+git clone https://github.com/hahwul/a2sv
+mv a2sv /data/data/com.termux/files/home
+git clone https://github.com/sandiwijayani1/SpamCall-1
+mv SpamCall-1 /data/data/com.termux/files/home
+git clone https://github.com/Tuhinshubhra/RED_HAWK
+mv RED_HAWK /data/data/com.termux/files/home
+git clone https://github.com/Cesar-Hack-Gray/SocialSploit
+mv SocialSploit /data/data/com.termux/files/home
+git clone https://github.com/hangetzzu/saycheese
+mv saycheese /data/data/com.termux/files/home
+git clone https://github.com/maldevel/IPGeoLocation
+mv IPGeoLocation /data/data/com.termux/files/home
+git clone https://github.com/abhinavkavuri/PhoneInfoga
+mv Phoneinfoga /data/data/com.termux/files/home
+git clone https://github.com/Hacking-pch/papaviruz
+mv papaviruz /data/data/com.termux/files/home
+git clone https://github.com/RealHackRH593/CC-MundoRH
+mv CC-MundoRH /data/data/com.termux/files/home
+git clone https://github.com/T3rMuX0/Consulta-v3
+mv Consulta-v3 /data/data/com.termux/files/home
+git clone https://github.com/Cesar-Hack-Gray/scam
+mv scam /data/data/com.termux/files/home
+git clone https://github.com/PSecurity/ps.ngrok
+mv ps.ngrok /data/data/com.termux/files/home
+git clone https://github.com/sqlmapproject/sqlmap
+mv sqlmap /data/data/com.termux/files/home
+git clone https://github.com/TermuxHacking000/PortmapSploit
+mv PortmapSploit /data/data/com.termux/files/home
+git clone https://github.com/penetrate2hack/ITWSV
+mv ITWSV /data/data/com.termux/files/home
+git clone https://github.com/uhusmanhaider/facebook-report
+mv facebook-report /data/data/com.termux/files/home
+git clone https://github.com/darkcurut08/darkcurut08
+mv darkcurut08 /data/data/com.termux/files/home
+git clone https://github.com/Cesar-Hack-Gray/FotoSploit
+mv FotoSploit /data/data/com.termux/files/home
+git clone https://github.com/aboul3la/Sublist3r
+mv Sublist3r /data/data/com.termux/files/home
+git clone https://github.com/rajkumardusad/IP-Tracer
+mv IP-Tracer /data/data/com.termux/files/home
+git clone https://github.com/RealHackRH593/BinsGenRH
+mv BinsGenRH /data/data/com.termux/files/home
+git clone https://github.com/suyash6/hulk
+mv hulk /data/data/com.termux/files/home
+git clone https://github.com/JoeTech-Studio/Bingen
+mv Bingen /data/data/com.termux/files/home
+git clone https://github.com/TermuxHacking000/PortmapSploit.git
+mv PortmapSploit.git /data/data/com.termux/files/home
+git clone https://github.com/penetrate2hack/ITWSV.git
+mv ITWSV.git /data/data/com.termux/files/home
+git clone https://github.com/blackcodercrush/hack-facebook-teman
+mv hack-facebook-teman /data/data/com.termux/files/home
+git clone https://github.com/dizart-y/cLONiNg-Yahoo
+mv Yahoo /data/data/com.termux/files/home
+git clone https://github.com/perjayro/MultiPhish.git
+mv MultiPhish.git /data/data/com.termux/files/home
+git clone https://github.com/luisakrac/WFIKRACC.git
+mv WFIKRACC.git /data/data/com.termux/files/home
+git clone https://github.com/threat9/routersploit
+mv routersploit /data/data/com.termux/files/home
+git clone https://github.com/Bl4ckDr460n/Black-Fb-Premium
+mv Black-Fb-Premium /data/data/com.termux/files/home
+git clone https://github.com/Bl4ckDr460n/G-Dork
+mv G-Dork /data/data/com.termux/files/home
+git clone https://github.com/Bl4ckDr460n/SPAMer
+mv SPAMer /data/data/com.termux/files/home
+git clone https://github.com/Bl4ckDr460n/My-Ip
+mv My-Ip /data/data/com.termux/files/home
+git clone https://github.com/Bl4ckDr460n/STrojans
+mv STrojans /data/data/com.termux/files/home
+git clone https://github.com/zlucifer/lucita_ddos
+mv lucita_ddos /data/data/com.termux/files/home
+git clone https://github.com/MrUncle/PulsaGratis
+mv PulsaGratis /data/data/com.termux/files/home
+git clone https://github.com/MrUncle/Script-HackBank
+mv HackBank /data/data/com.termux/files/home
+git clone https://github.com/T14RB373T/FAKECALL
+mv FAKECALL /data/data/com.termux/files/home
+clear
+printf $COR_VERDE
+echo "===================================="
+echo  "[•]Created by:Z3RØ_AN7RÄX"
+echo "===================================="
+printf $COR_AMARELO
+echo "           ┌∩┐(◣_◢)┌∩┐"
+printf $COR_VERDE
+echo "===================================="
+echo "           [•]PRONTO"
+echo "[•]Créditos:Senhor Loock & NEAR SHELBY"
+echo "[•]👾Iꈤᐯꀤꌗꀤꂦꈤ Mꍏꌗ꓄ꍟꋪ👾"
